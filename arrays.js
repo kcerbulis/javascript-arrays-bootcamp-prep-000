2870e8978cb08e19d1402832c9b4257085efcd74
reactive-queue-1834 <kcerbulis@gmail.com>
2019-02-06 22:11:51 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -36,12 +36,3 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
   return array
 }
 
-function removeElementFromBeginningOfArray(array){
-  array.slice(-1)
-  return array
-}
-
-function removeElementFromBeginningOfArray(array){
-  array.shift(1)
-  return array
-}
