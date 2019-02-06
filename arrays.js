42ed09fb7bec6d9e49eaf51be1910e105c602c00
reactive-queue-1834 <kcerbulis@gmail.com>
2019-02-06 21:47:00 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,23 @@
+var chocolateBars = new Array("snickers", "hundred grand", "kitkat", "skittles")
+
+
+
+function addElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
+}
+
+function addElementToBeginningOfArray(array, element){
+  array = [element, ...array]
+  return array
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element){
+  array = [...array, element]
+  return array
+}
